69dcd8abec351088c8e7f289825228a4479f857c
cfamilyit <96969060+cfamilyit@users.noreply.github.com>
2022-12-15 22:36:11 +0530

    Add files via upload

---
+++ python_examples/arithmetic.py
@@ -0,0 +1,26 @@
+a = 10
+b = 5
+
+def sum():
+	res = a+b
+	print("A is:",a)
+	print("B is:",b)
+	print("Sum of A,B is:",res)
+	
+def sub():
+	res = a-b
+	print("A is:",a)
+	print("B is:",b)
+	print("Subtraction of A,B is:",res)
+	
+def mul():
+	res = a*b
+	print("A is:",a)
+	print("B is:",b)
+	print("Multiplication of A,B is:",res)
+	
+def div():
+	res = a//b
+	print("A is:",a)
+	print("B is:",b)
+	print("Division of A,B is:",res)
